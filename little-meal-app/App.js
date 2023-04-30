0947d7e0f55fce2ee446b9aa107e2060a25723de
podnesTaF <podnestrade@gmail.com>
2023-04-30 22:37:28 +0200

    finish meal app + drawer sumple

--- little-meal-app/App.js
+++ little-meal-app/App.js
@@ -5,8 +5,30 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import {createDrawerNavigator} from "@react-navigation/drawer";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+const DrawerNavigator = () => {
+    return <Drawer.Navigator screenOptions={{
+        headerStyle: {
+            backgroundColor: '#391404',
+        },
+        headerTintColor: '#fff',
+        sceneContainerStyle: {
+            backgroundColor: '#703232',
+        },
+        drawerContentStyle: {
+            backgroundColor: '#703232',
+        },
+        drawerActiveTintColor: '#fff',
+        drawerInactiveTintColor: '#e2b497',
+    }}>
+        <Drawer.Screen name={"Categories"} component={CategoryScreen}/>
+        <Drawer.Screen name={"Favorites"} component={FavoritesScreen }/>
+    </Drawer.Navigator>
+}
 
 export default function App() {
   return (
@@ -22,8 +44,9 @@ export default function App() {
                  backgroundColor: '#703232',
              }
          }}>
-                <Stack.Screen name={"Categories"} component={CategoryScreen} options={{
+                <Stack.Screen name={"Drawer"} component={DrawerNavigator} options={{
                     title: 'All Categories',
+                    headerShown: false,
                 }} />
              <Stack.Screen name={'MealsOverview'} component={MealsOverviewScreen}/>
              <Stack.Screen name={'MealDetail'} component={MealDetailScreen}/>
---
+++ little-meal-app/screens/FavoritesScreen.js
@@ -0,0 +1,11 @@
+import {View, Text} from "react-native";
+
+const FavoritesScreen = () => {
+    return (
+        <View>
+            <Text>Hello this is my favorites</Text>
+        </View>
+    );
+};
+
+export default FavoritesScreen;
