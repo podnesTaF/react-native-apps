4213637717e121f863be8c48b9c182e05132ec06
podnesTaF <podnestrade@gmail.com>
2023-05-05 08:35:43 +0200

    adding env variables

--- auth-with-rn/util/auth.js
+++ auth-with-rn/util/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = 'AIzaSyCoGWkWWrEuWo_kPXohE7Oq_3mwIxcklTM';
+const API_KEY = process.env.API_KEY;
 
 export const authenticate = async (mode, email, password) => {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`
